debaed702c9cce5253135aeb96e05f7daa36f4da
joroinnoroff <joroin49311@stud.noroff.no>
2024-07-17 17:06:18 +0200

    Fix: Ensure correct error handling in register function for 400 error and jest test

--- src/js/api/auth/register.js
+++ src/js/api/auth/register.js
@@ -1,16 +1,18 @@
 import { apiPath } from "../constants.js";
 import { headers } from "../headers.js";
 
-export async function register(name, email, password, avatar, banner ) {
-	const response = await fetch(`${apiPath}/social/auth/register`, {
-		method: "post",
-		body: JSON.stringify({ name, email, password, avatar, banner  }),
-		headers: headers("application/json"),
-	});
+export async function register(name, email, password, avatar, banner) {
+  const response = await fetch(`${apiPath}/social/auth/register`, {
+    method: "post",
+    body: JSON.stringify({ name, email, password, avatar, banner }),
+    headers: headers("application/json"),
+  });
 
-	if (response.ok) {
-		return await response.json();
-	}
+  if (response.ok) {
+    return await response.json();
+  }
 
-	throw new Error(response.statusText);
+  const error = new Error(response.statusText);
+  error.response = response;
+  throw error;
 }
